386e766c9466321910d875045750b879fd6d7036
lauer3912 <lauer3912@gmail.com>
2024-04-04 17:40:49 +0800

    优化init.js

--- init.js
+++ init.js
@@ -7,9 +7,20 @@
 const fs = require('fs');
 const path = require('path');
 
-// 子域名，克隆后，可以修改为自己的子域名
-const subDomainName = 'android-transfer'; 
+// ------------------------------------------------------------------------------------------- 以下内容需要自己替换
+const subSiteAuthor = 'Nova';
+const subSiteKeywords = `` + ','; // 关键字，多个关键字用逗号分隔
+const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0'; // 替换为你的API密钥
 
+
+
+// 子域名，克隆后，可以修改为自己的子域名. 根据目录自动处理了。
+const subDomainName = path.basename(__dirname).replace('site-', ''); 
+const subSiteTitle = subDomainName.replaceAll('-', ' ').replace(/\b\w/g, (char) => {return char.toUpperCase()}); // 网站标题, 每个字母第一个字符都要大写
+
+
+
+//----------------------------------------------------------------------------------------------
 function replaceFileContent(filePath) {
   const ext = path.extname(filePath);
   const excludeList = ['.jpg', '.jpeg', '.png'];
@@ -21,6 +32,20 @@ function replaceFileContent(filePath) {
   const content = fs.readFileSync(filePath, 'utf-8');
   let newContent = content.replaceAll(`android-transfer.techidaily.com`, `${subDomainName}.techidaily.com`);
   newContent = newContent.replaceAll(`site-android-transfer.git`, `site-${subDomainName}.git`);
+  newContent = newContent.replaceAll(`hexo-site-android-transfer`, `hexo-site-${subDomainName}`);
+
+  // 标题替换
+  newContent = newContent.replaceAll(`title: TechiDaily`, `title: ${subSiteTitle}`);
+  newContent = newContent.replaceAll(`description: 'TechiDaily is`, `description: '${subSiteTitle} is`);
+
+  // 作者替换
+  newContent = newContent.replaceAll(`author: Nova`, `author: ${subSiteAuthor}`);
+
+  // 关键字替换
+  newContent = newContent.replaceAll(`keywords: 'Tools, `, `keywords: '${subSiteKeywords} Tools, `);
+
+  // IndexNow API Key 替换
+  newContent = newContent.replaceAll(`const indexNowKey = 'ec9c9516ea82433086d979e16e1960e0';`, `const indexNowKey = '${indexNowKey}';`);
 
   fs.writeFileSync(filePath, newContent);
 }
@@ -41,3 +66,15 @@ function replaceFile(filePath) {
 replaceFile(path.join(__dirname, 'source'));
 replaceFile(path.join(__dirname, '_config.yml'));
 replaceFile(path.join(__dirname, '_config.redefine.yml'));
+
+replaceFile(path.join(__dirname, 'package.json'));
+replaceFile(path.join(__dirname, 'submit.bing.js'));
+
+try {
+  const newApiKeyFile = path.join(__dirname, 'source', `${indexNowKey}.txt`);
+  // 将API Key 内容写入到 source/your-api-key.txt 文件中
+  fs.writeFileSync(newApiKeyFile, indexNowKey);
+} catch (e){
+  console.error(e);
+}
+
